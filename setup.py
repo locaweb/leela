f4be85733ad215cb820a7ede7e232b9c535c4d3f
Juliano Martinez <juliano@falaserio.org>
2012-04-09 18:37:33 -0300

    leela servr

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+from distutils.core import setup
+
+setup(name='leela-server',
+    version='0.0.1',
+    description='Collect and Chart anything',
+    author='Juliano Martinez',
+    author_email='juliano@martinez.io',
+    url='https://github.com/ncode/leela-server',
+    install_requires=[
+        'gevent>=0.13.6'],
+    packages=['leela-server'],
+    package_dir={'leela-server': 'src/lib'},
+    data_files=[('/usr/sbin', ['src/sbin/leela-server']),
+                ('/etc/leela', ['src/etc/leela.conf']),
+                ('/usr/share/leela/templates', ['src/share/templates/line-irregular.tmpl']),
+        ]
+    )
