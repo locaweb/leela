84f62cb7f2d9bdda381303e2b1d60ed3ab195809
Juliano Martinez <juliano@falaserio.org>
2012-04-30 16:54:25 -0300

    cleaning and organizing code

---
+++ src/lib/config.py
@@ -0,0 +1,14 @@
+import os
+import ConfigParser
+
+config = ConfigParser.ConfigParser()
+if ("LEELA_CFG" in os.environ):
+    config.read(os.environ["LEELA_CFG"])
+else:
+    config.read("/etc/leela/leela.conf")
+
+def get(*args, **kwargs):
+    return(config.get(*args, **kwargs))
+
+def getint(*args, **kwargs):
+    return(config.getint(*args, **kwargs))
--- src/lib/lasergun.py
+++ src/lib/lasergun.py
@@ -15,8 +15,150 @@
 #
 # @author: Juliano Martinez
 
-import json
+from gevent import monkey
+monkey.patch_all()
+
+import re
+import sys
+import time
+import syslog
+import pycassa
+import ConfigParser
+from leela import config
+from datetime import datetime
+from hotqueue import HotQueue
+from pycassa.types import LongType
+from pycassa.types import UTF8Type
+from pycassa.types import DoubleType
 from Cheetah.Template import Template
+from pycassa.pool import ConnectionPool
+from pycassa.types import CompositeType
+from pycassa.system_manager import SystemManager
+
+syslog.openlog(sys.argv[0].split('/')[-1], syslog.LOG_PID, syslog.LOG_DAEMON)
+cassandra = config.get('cassandra','server')
+
+queue = HotQueue(
+    config.get('hotqueue','queue'),
+    host=config.get('hotqueue','host'),
+    port=config.getint('hotqueue','port'),
+    db=config.getint('hotqueue','db')
+)
+
+allColumnFamilyOptions = {
+    'default_validation_class': DoubleType(),
+    'key_cache_size': 9000,
+    'row_cache_size': 1200
+}
+
+pool = pycassa.ConnectionPool(
+    keyspace=config.get('cassandra','keyspace'),
+    server_list=[cassandra],
+    pool_size=20,
+    prefill=True
+)
+
+def prepare_cf(srtftime, hostname, service, timestamp):
+    systemManager = pycassa.system_manager.SystemManager(cassandra, timeout=10)
+    columnFamily = "%s_%s_%s" % (
+            hostname,
+            service,
+            datetime.fromtimestamp(timestamp).strftime(srtftime)
+    )
+
+    for count in range(config.getint('cassandra','retries')):
+        columnFamilies = systemManager.get_keyspace_column_families('leela')
+        if columnFamily not in columnFamilies:
+            try:
+                systemManager.create_column_family(
+                    'leela',
+                    columnFamily,
+                    **allColumnFamilyOptions
+                )
+                syslog.syslog('Creating Cassandra keyspace %s' % columnFamily)
+                break
+            except pycassa.cassandra.ttypes.SchemaDisagreementException:
+                syslog.syslog('Problem creating %s, retrying' % (columnFamily))
+                sleep(1)
+            except pycassa.cassandra.ttypes.InvalidRequestException:
+                syslog.syslog('Column Family %s, already existis' % (columnFamily))
+                break
+            except Exception, e:
+                syslog.syslog('Exception %s on %s' % (e, columnFamily))
+    return pycassa.ColumnFamily(pool, columnFamily)
+
+def summarize(data):
+    line, timestamp = data
+
+    parsed = {}
+    hostname, service = line.split('||')[0].split('|')
+    service = service.capitalize()
+
+    def accounting(values, name):
+        total = 0
+        count = 0
+        for _, data in values:
+            for key, value in data.iteritems():
+                if key.startswith('%s||' % name):
+                    total = total + float(value)
+                    count = count + 1
+
+        try:
+            return total / float(count)
+        except ZeroDivisionError:
+            return 0
+
+    for srtftime in ["%Y%m%d%H", "%Y%m%d"]:
+        cf = prepare_cf(srtftime[:-2], hostname, service, timestamp)
+        date = datetime.fromtimestamp(timestamp)
+        _ts = time.mktime(
+            datetime(
+                year=date.year,
+                month=date.month,
+                day=date.day,
+                hour=date.hour
+                    if srtftime == "%Y%m%d%H" else 0
+            ).timetuple()
+        )
+
+        for data in line.split('||')[1:]:
+            name, _ = data.split('|')
+            total = 0
+            try:
+                client = pycassa.ColumnFamily(
+                    pool, '%s_%s_%s' % (
+                        hostname,
+                        service,
+                        date.strftime(srtftime)
+                    )
+                )
+                values = client.get_range(column_count=1000)
+                total = accounting(values, name)
+                cf.insert(service, {"%s||%s" % (name, _ts): total})
+            except Exception, e:
+                print e
+            syslog.syslog("%s - %s||%s - %s" % (
+                service, name, date.strftime(srtftime), total)
+            )
+
+def parse_and_save_datagram(line):
+    systemManager = pycassa.system_manager.SystemManager(cassandra, timeout=10)
+
+    parsed = {}
+    hostname, service = line.split('||')[0].split('|')
+    service = service.capitalize()
+
+    now = datetime.now()
+    timestamp = time.time()
+    queue.put((line, timestamp))
+    cf = prepare_cf("%Y%m%d%H", hostname, service, timestamp)
+    for data in line.split('||')[1:]:
+        name, value = data.split('|')
+        try:
+            cf.insert(service, {"%s||%s" % (name, timestamp): float(value)})
+        except Exception, e:
+            print e
+        syslog.syslog("%s - %s||%s - %s" % (service, name, timestamp, float(value)))
 
 def render_template(template, service, hostname, series):
     render = Template(file = template,
