2b3d5d393d49039bd32556024e3b51f8e151ea94
diego souza <dsouza@c0d3.xxx>
2012-07-23 19:12:35 -0300

    adding datafiles into the package

--- setup.py
+++ setup.py
@@ -4,19 +4,30 @@ import os
 from setuptools import find_packages
 from setuptools import setup
 
-setup( name             = "leela-server",
-       version          = "0.0.12",
-       description      = "Collect and Chart anything",
-       author           = "Juliano Martinez, Diego Souza",
-       author_email     = "juliano@martinez.io",
-       url              = "https://github.com/ncode/leela-server",
-       install_requires = ["gevent>=0.13.6", "pycassa", "bottle", "supay", "argparse"],
-       packages         = find_packages("src"),
-       package_dir      = {"": "src"},
-       data_files       = [],
-       entry_points     = {
-           "console_scripts": [
-               "leela-server = leela.lasergun.server:main",
-               "leela-web    = leela.readata.server:main"
-           ],
-     )
+def find_datafiles(root, path_f):
+    result = []
+    for (root, dirname, files) in os.walk(root):
+        tr_root = path_f(root)
+        if (tr_root != False):
+            result.append((tr_root, map(lambda f: os.path.join(root, f), files)))
+    return(result)
+
+accept_f = lambda f: reduce(lambda acc, p: acc or f.startswith(p), ("./etc", "./usr"), False)
+path_f   = lambda f: accept_f(f) and f[1:] or False
+setup(
+    name             = "leela-server",
+    version          = "0.0.12",
+    description      = "Collect and Chart anything",
+    author           = "Juliano Martinez, Diego Souza",
+    author_email     = "juliano@martinez.io",
+    url              = "https://github.com/ncode/leela-server",
+    install_requires = ["gevent>=0.13.6", "pycassa", "bottle", "supay", "argparse"],
+    packages         = find_packages("src"),
+    package_dir      = {"": "src"},
+    data_files       = find_datafiles(".", path_f),
+    entry_points     = {
+        "console_scripts": [
+            "leela-server = leela.lasergun.server:main",
+            "leela-web    = leela.readata.server:main"
+        ],
+    })
