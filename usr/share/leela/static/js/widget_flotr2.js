7497c83b6efcf3c4c79454dcfea7e7aca170c67a
diego souza <dsouza@c0d3.xxx>
2012-08-14 09:56:09 -0300

    WIP: cspline

--- usr/share/leela/static/js/widget_flotr2.js
+++ usr/share/leela/static/js/widget_flotr2.js
@@ -7,12 +7,48 @@ if (LEELA === undefined) {
 LEELA.widget = function (root, opts) {
     var options = opts || {};
 
+    var cspline_p = function (x, xk_1, yk_1, xk, yk, xk1, yk1, xk2, yk2) {
+      var t   = (x-xk) / (xk1 - xk);
+      var t2  = t*t;
+      var t3  = t2*t;
+      var h00 = 2*t3 - 3*t2 + 1;
+      var h10 = t3 - 2*t2 + t;
+      var h01 = -2*t3 + 3*t2;
+      var h11 = t3 - t2;
+      var m0  = (yk1-yk)/(2*(xk1 - xk)) + (yk-yk_1)/(2*(xk-xk_1));
+      var m1  = (yk2-yk1)/(2*(xk2 - xk1)) + (yk1-yk)/(2*(xk1-xk));
+      return([x, h00*yk + h10*(xk1-xk)*m0 + h01*yk1 + h11*(xk1-xk)*m1]);
+    };
+
+    var cspline = function (data) {
+      var ndata = [];
+      var len   = data.length;
+      var res   = 100;
+
+      for (var k=0; k<len-1; k+=1) {
+        for (var u=1; u<res; u+=1) {
+          var x = k + u/res;
+          ndata.push(cspline_p(x,
+                               k-1,
+                               (data[k-1] || [0,0])[1],
+                               k,
+                               data[k][1],
+                               k+1,
+                               data[k+1][1],
+                               k+2,
+                               (data[k+2] || [0,0])[1]));
+        }
+      }
+
+      return(ndata);
+    };
+
     var format = function (json) {
       var series = [];
       for (var k in json) {
         if (k!=="source" && json.hasOwnProperty(k)) {
           series.push({ label: k,
-                        data: json[k]
+                        data: cspline(json[k])
                       });
         }
       }
@@ -32,10 +68,10 @@ LEELA.widget = function (root, opts) {
         options.subtitle = options.subtitle || "Powered by locaweb";
 
         options.xaxis             = options.xaxis || {};
-        options.xaxis.mode        = "time";
-        options.xaxis.labelsAngle = 45;
-        options.xaxis.timeFormat  = "%H:%M";
-        options.xaxis.timeUnit    = "second";
+        options.xaxis.mode        = "normal";
+        // options.xaxis.labelsAngle = 45;
+        // options.xaxis.timeFormat  = "%H:%M";
+        // options.xaxis.timeUnit    = "second";
 
         Flotr.draw(container, series, options);
 
