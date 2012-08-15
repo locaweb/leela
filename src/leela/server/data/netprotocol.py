7cabee34f08a2a209863dcc702c769c62eb21830
diego souza <dsouza@c0d3.xxx>
2012-08-15 16:58:09 -0300

    making sure parsing is using base10

--- src/leela/server/data/netprotocol.py
+++ src/leela/server/data/netprotocol.py
@@ -24,10 +24,11 @@ def parse1(string):
     (name, value) = string.split(": ", 2)
     if (" " in value):
         (lval, tval) = value.split(" ", 2)
+        tval = long(tval, 10)
     else:
         lval = value
-        tval = time.time()
-    return(Event(name[:255], float(lval), long(tval)))
+        tval = long(time.time())
+    return(Event(name[:255], float(lval), tval))
 
 def parse(string):
     return(map(parse1, string.splitlines()))
