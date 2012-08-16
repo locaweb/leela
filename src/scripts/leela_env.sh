1818bd94dc424deb65528cfcc2d319b69696cc48
diego souza <dsouza@c0d3.xxx>
2012-08-16 09:32:35 -0300

    bugfix: using etc/leela/leela.conf

--- src/scripts/leela_env.sh
+++ src/scripts/leela_env.sh
@@ -2,7 +2,7 @@
 
 leela_root=${leela_root:-$(pwd)}
 leela_pypath="$leela_root/src"
-leela_cfg="$leela_root/etc/leela.conf"
+leela_cfg="$leela_root/etc/leela/leela.conf"
 
 echo "leela_env: [root: $leela_root]" >&2
 echo "  PYTHONPATH: $leela_pypath"    >&2
