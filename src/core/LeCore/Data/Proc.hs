1aab4c139a177e27f3f48e3e428255649f734df2
diego souza <dsouza@c0d3.xxx>
2012-09-05 09:32:54 -0300

    creating chunk type

--- src/core/LeCore/Data/Proc.hs
+++ src/core/LeCore/Data/Proc.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 -- All Rights Reserved.
 --
 --    Licensed under the Apache License, Version 2.0 (the "License");
@@ -27,24 +28,43 @@ module LeCore.Data.Proc
        -- ^ Utility procs
        , pure
        , window
+       -- ^ Chunk related
+       , fromList
+       , toList
        ) where
 
-data Proc i o = Done o
-              | Cont (i -> Proc i o)
+import Data.Monoid
 
--- | Produces a value
+data Proc i o = Put o
+              | Get (i -> Proc i o)
+
+data Chunk i = Chunk { val :: i
+                     , eof :: Bool
+                     }
+
+-- | Produces a value, usually meaning the proc is done
 done :: o -> Proc i o
-done = Done
+done = Put
 
 -- | Requests more input
 await :: (i -> Proc i o) -> Proc i o
-await = Cont
+await = Get
+
+suffixR :: (a -> [a] -> b) -> [a] -> [b]
+suffixR _ []     = []
+suffixR f (x:xs) = f x xs : suffixR f xs
+
+fromList :: [a] -> [Chunk a]
+fromList = suffixR (\c -> Chunk c . null)
+
+toList :: [Chunk a] -> [a]
+toList = map val
 
 -- | Feed everything into the proc until no more input is
 -- available. Notice the output may not be a 1:1 correspondence in
 -- to the input.
-run :: Proc i o -> [i] -> [o]
-run f = go f f
+run :: Proc (Chunk i) o -> [i] -> [o]
+run f = go f f . fromList
   where go _ _ []     = []
         go z g (x:xs) = case (eval g x)
                         of Right (o, Nothing) -> o : go z z xs
@@ -55,15 +75,15 @@ run f = go f f
 -- produced a value, with a possibly leftover. Left is returned when
 -- the process is requesting more input.
 eval :: Proc i o -> i -> Either (Proc i o) (o, Maybe i)
-eval (Done o) i = Right (o, Just i)
-eval (Cont f) i = case f i
-                  of Done o -> Right (o, Nothing)
+eval (Put o) i = Right (o, Just i)
+eval (Get f) i = case f i
+                  of Put o -> Right (o, Nothing)
                      h      -> Left h
 
 -- | Apply a pure function over the proc.
 apply :: (a -> b) -> Proc i a -> Proc i b
-apply f (Done a) = done (f a)
-apply f (Cont g) = await (\i -> apply f (g i))
+apply f (Put a) = done (f a)
+apply f (Get g) = await (\i -> apply f (g i))
 
 -- | Connects two procs. It fully evaluatates the first proc and its
 -- result is fed up into the second proc. Notice that if the second
@@ -71,10 +91,10 @@ apply f (Cont g) = await (\i -> apply f (g i))
 pipe :: Proc a a  -- ^ The first proc
      -> Proc a a  -- ^ The second proc
      -> Proc a a
-pipe _ (Done a)           = done a
-pipe (Done a) (Cont f)    = f a
-pipe (Cont f) g0@(Cont g) = await (\i -> case (f i)
-                                         of Done a -> g a
+pipe _ (Put a)          = done a
+pipe (Put a) (Get f)    = f a
+pipe (Get f) g0@(Get g) = await (\i -> case (f i)
+                                         of Put a -> g a
                                             h      -> h `pipe` g0)
 
 -- | Uses a pure function as a proc.
@@ -84,13 +104,24 @@ pure f = await (done . f)
 -- | Groups n items together and use a function to produce a value.
 window :: Int          -- ^ How many items to group
        -> ([i] -> o)   -- ^ The function to use to produce a value
-       -> Proc i o
+       -> Proc (Chunk i) o
 window n f = go []
   where go !acc
           | length acc == n = done (f acc)
-          | otherwise       = await (\i -> go (i:acc))
+          | otherwise       = await (\i -> let v = val i
+                                           in if (eof i)
+                                              then done (f $ v : acc)
+                                              else go (v:acc))
 
 instance Functor (Proc i) where
 
   fmap = apply
 
+instance Functor Chunk where
+
+  fmap f c = c { val = f (val c) }
+
+instance (Monoid i) => Monoid (Chunk i) where
+
+  mempty      = Chunk mempty False
+  mappend a b = Chunk (val a `mappend` val b) (eof a || eof b)
