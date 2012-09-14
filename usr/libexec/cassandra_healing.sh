7b3d1e233333b96ae7a40e62a94e5708e24bd329
diego souza <dsouza@c0d3.xxx>
2012-09-14 09:36:15 -0300

    only compacting on full mode on

--- usr/libexec/cassandra_healing.sh
+++ usr/libexec/cassandra_healing.sh
@@ -3,8 +3,8 @@
 if [ "$1" == "full" ]
 then
   nodetool -h localhost repair
+  nodetool -h localhost compact
 else
   nodetool -h localhost repair -pr
 fi
-nodetool -h localhost compact
 nodetool -h localhost cleanup
