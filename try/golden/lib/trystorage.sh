b3f2ffb6f44cfb88192cd41cd7f6ac2672a0cc00
diego souza <dsouza@c0d3.xxx>
2012-10-24 17:52:43 -0200

    using stdin to make tests more readable

--- try/golden/lib/trystorage.sh
+++ try/golden/lib/trystorage.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-leela_trystorage_1 () {
+leela_trystorage_write_to_fifo_and_enum_from_cassandra () {
   leela_trylib_fifo_drop
   leela_trylib_fifo_creat
   leela_trylib_cassandra_drop >/dev/null
   leela_trylib_cassandra_creat >/dev/null
   
   leela_trylib_service_start storage >/dev/null
-  echo -n "event 6|foobar 1351101449.0 0.75;" | leela_trylib_fifo_write
-  echo "list events;" | leela_trylib_cassandra_execute | grep column=
+  leela_trylib_fifo_write
+  echo "list events;" | leela_trylib_cassandra_execute
 
   leela_trylib_service_stop
 }
--- try/golden/lib/tryudp.sh
+++ try/golden/lib/tryudp.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-leela_tryudp_1 () {
+leela_tryudp_write_to_socket_and_read_from_fifo () {
   leela_trylib_fifo_drop
   leela_trylib_fifo_creat
   leela_trylib_service_start udp >/dev/null
 
-  echo "foobar: 0.75 1351101449" | leela_trylib_udp_write
+  leela_trylib_udp_write
   leela_trylib_fifo_read; echo
 
   leela_trylib_service_stop
