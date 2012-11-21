8bc22cae71c03545ad715e191bd1b156d58b9e62
dgvncsz0f <dsouza@c0d3.xxx>
2012-11-21 15:52:24 -0200

    bugfix: changelog script no longer duplicates entries on tag boundaries

--- src/scripts/changelog.sh
+++ src/scripts/changelog.sh
@@ -22,8 +22,18 @@ prepend () {
   rm -f "$src.0" "$chk"
 }
 
+extract () {
+  commit=$1
+  prefix=$(gitlog -n1 --pretty=format:%s $commit | sed s,/,,g)
+  suffix=$(gitlog -n1 --pretty=format:"| %aN (%ai)" $commit | sed s,/,,g)
+  gitlog -n1 --pretty=format:"%b%n%N" $commit    | \
+    grep '^\[changelog\]'                  | \
+    sed -r "s/^\\[changelog\\] *\$/  * $prefix/; s/^\\[changelog\\] /  * /; s/\$/ $suffix/"
+}
+
 payload () {
   tag0=$(git --no-pager rev-list --max-parents=0 HEAD)
+  fst=true
   for tag in $(git tag | grep '^v' | sort -V)
   do
     hdr=$(gitlog -n1 --pretty=format:"$tag | (%ai)" $tag)
@@ -31,14 +41,16 @@ payload () {
     echo "$hdr"                >>"$TMPFILE.1"
     echo "$hdr" | sed s/./-/g  >>"$TMPFILE.1"
 
-    for commit in $tag0 $(gitlog --pretty=format:"%H" $tag0..$tag)
+    if [ "$fst" = true ]
+    then
+      fst=false
+      extract $tag0 >>"$TMPFILE.1"
+    fi
+
+    for commit in $(gitlog --pretty=format:"%H" $tag0..$tag)
     do
-      prefix=$(gitlog -n1 --pretty=format:%s $commit | sed s,/,,g)
-      suffix=$(gitlog -n1 --pretty=format:"| %aN (%ai)" $commit | sed s,/,,g)
-      gitlog -n1 --pretty=format:"%b%n%N" $commit    | \
-        grep '^\[changelog\]'                  | \
-        sed -r "s/^\\[changelog\\] *\$/  * $prefix/; s/^\\[changelog\\] /  * /; s/\$/ $suffix/"
-    done >>"$TMPFILE.1"
+      extract $commit >>"$TMPFILE.1"
+    done
 
     prepend "$TMPFILE" "$TMPFILE.1"
     tag0=$tag
