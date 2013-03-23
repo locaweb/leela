b94ec67b46e8c700b5168292a0589aba3aa9eacd
dgvncsz0f <dsouza@c0d3.xxx>
2013-03-23 11:18:39 -0300

    turning on dired-omit-mode by default

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
-((python-mode
-  (py-indent-offset . 4)))
+((python-mode (py-indent-offset . 4))
+ (dired-mode) (dired-omit-mode 1))
