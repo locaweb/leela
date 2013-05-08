eb832b4655ae8d7b4ee408db87791bba7254b342
dgvncsz0f <dsouza@c0d3.xxx>
2013-05-08 18:16:56 -0300

    embedding python-twisted and python-wokkel as debian stable provides no suitable version

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -32,16 +32,24 @@ install_debian () {
     --install-layout=deb                    \
     --root="$srcroot/debian/tmp"            \
     --record="$record"
-  for u in http://pypi.python.org/packages/source/t/txredisapi/txredisapi-1.0.tar.gz \
-           http://pypi.python.org/packages/source/c/cyclone/cyclone-1.1.tar.gz       \
-           http://pypi.python.org/packages/source/t/thrift/thrift-0.9.0.tar.gz       \
+  for u in http://pypi.python.org/packages/source/T/Twisted/Twisted-13.0.0.tar.bz2             \
+           http://pypi.python.org/packages/source/z/zope.interface/zope.interface-4.0.3.tar.gz \
+           http://pypi.python.org/packages/source/w/wokkel/wokkel-0.7.1.tar.gz                 \
+           http://pypi.python.org/packages/source/t/txredisapi/txredisapi-1.0.tar.gz           \
+           http://pypi.python.org/packages/source/c/cyclone/cyclone-1.1.tar.gz                 \
+           http://pypi.python.org/packages/source/t/thrift/thrift-0.9.0.tar.gz                 \
            https://github.com/driftx/Telephus/tarball/releases/1.0.0_beta1
   do
     oldpath=$(pwd)
     TMPDIR=$(mktemp -d) && cd "$TMPDIR" && {
       file="$TMPDIR"/$(basename "$u")
       wget -O"$file" "$u"
-      tar -x -z --xform 's,[^/]*/,,' -f "$file"
+      if echo $u | grep -q Twisted
+      then
+        tar -x -j --xform 's,[^/]*/,,' -f "$file"
+      else
+        tar -x -z --xform 's,[^/]*/,,' -f "$file"
+      fi
       "$bin_python" setup.py install \
         --install-layout=deb         \
         --root="$srcroot/debian/tmp" \
