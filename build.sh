82449d1bdcb51d83a909b70aefba37ce614f068f
dgvncsz0f <dsouza@c0d3.xxx>
2013-09-13 18:58:51 -0300

    using docker id instead of tags for now

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@ trap cleanup INT
 cd $(dirname $(readlink -f "$0"))
 
 sudo=""
-[ "$UID" -eq 0 ] || sudo="sudo"
+[ "$(id -u)" -ne 0 ] || sudo="sudo"
 docker="$sudo docker"
 
 sendfile () {
   for _ in $(seq 1 60)
   do
     sleep 1
-    if lsof -t -itcp:9999
+    if $sudo lsof -t -itcp:9999
     then break; fi
   done
   socat -u OPEN:"$1" TCP-CONNECT:127.0.0.1:9999 2>/dev/null
@@ -21,7 +21,7 @@ sendfile () {
 
 build_image () {
   echo "starting builder ..."
-  $docker run -a stdout -p 9999:9999 leela:dev /bin/leela-recv-build >docker/app/leela-dist.tgz
+  $docker run -a stdout -p 9999:9999 7ae5921a2397 /bin/leela-recv-build >docker/app/leela-dist.tgz
 }
 
 build_app () {
