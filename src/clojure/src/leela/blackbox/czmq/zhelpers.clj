f0bcbf514c8a6405739dc757e2f74e533a30c7ea
dgvncsz0f <dsouza@c0d3.xxx>
2013-09-26 16:36:17 -0300

    adding license header everywhere o.O

--- src/clojure/src/leela/blackbox/czmq/zhelpers.clj
+++ src/clojure/src/leela/blackbox/czmq/zhelpers.clj
@@ -1,3 +1,18 @@
+;; This file is part of Leela.
+;;
+;; Leela is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; Leela is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with Leela.  If not, see <http://www.gnu.org/licenses/>.
+
 (ns leela.blackbox.czmq.zhelpers
   (:import [org.zeromq ZMQ ZMQ$PollItem ZMQ$Socket]))
 
