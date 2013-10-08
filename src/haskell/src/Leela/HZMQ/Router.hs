39a42fad39c198c78ac15265845d96f16976d544
dgvncsz0f <dsouza@c0d3.xxx>
2013-10-08 09:15:45 -0300

    forking on worker per request, as we don't want to run dry on workers
    Need to benchmark this as creating new sockets everything may be
    cost. If this is the case, we should maintain a pool of workers that
    is allowed to grow when necessary.

--- src/haskell/src/Leela/HZMQ/Router.hs
+++ src/haskell/src/Leela/HZMQ/Router.hs
@@ -30,6 +30,7 @@ import           Leela.Helpers
 import           Leela.Data.Time
 import qualified Data.ByteString as B
 import           Control.Exception
+import           Control.Concurrent
 import           Data.List.NonEmpty
 import           Leela.Data.QDevice
 import           Leela.HZMQ.ZHelpers
@@ -66,18 +67,17 @@ logresult job me = do
       failOrSucc Nothing  = "ROUTER.ok"
       failOrSucc (Just e) = printf "ROUTER.fail[%s]" (show e)
 
-request :: Device Request -> Request -> IO ()
-request queue req = devwriteIO queue req
+-- request :: Device Request -> Request -> IO ()
+-- request queue req = devwriteIO queue req
 
-dequeueReq :: Device Request -> IO Request
-dequeueReq queue = devreadIO queue
+-- dequeueReq :: Device Request -> IO Request
+-- dequeueReq queue = fmap fromJust $ devreadIO queue
 
 reply :: Request -> Socket Push -> [B.ByteString] -> IO ()
 reply job fh msg = sendMulti fh (fromList $ readPeer job : "" : msg)
 
-worker :: Device Request -> Socket Push -> Worker -> IO ()
-worker queue fh action = do
-  job  <- dequeueReq queue
+worker :: Request -> Socket Push -> Worker -> IO ()
+worker job fh action = do
   mmsg <- try (onJob action (readMsg job))
   case mmsg of
     Left e    -> do
@@ -88,13 +88,12 @@ worker queue fh action = do
       logresult job Nothing
       reply job fh msg
   
-forkWorker :: Control -> Context -> String -> Device Request -> Worker -> IO ()
-forkWorker ctrl ctx addr queue action =
-  forkOSSupervised (fmap not $ closed ctrl) $ do
-    withSocket ctx Push $ \fh -> do
-      connect fh addr
-      configure fh
-      forever (worker queue fh action)
+forkWorker :: Context -> String -> Request -> Worker -> IO ()
+forkWorker ctx addr job action = forkIO (do
+  withSocket ctx Push $ \fh -> do
+    connect fh addr
+    configure fh
+    forever (worker job fh action)) >> return ()
 
 recvRequest :: Receiver a => Socket a -> IO (Maybe Request)
 recvRequest fh = do
@@ -118,20 +117,18 @@ startRouter ctrl name cfg action ctx = do
       bind ifh (endpoint cfg)
       configure ifh
       configure ofh
-      queue <- openIO ctrl (queueSize cfg)
-      replicateM_ (capabilities cfg) (forkWorker ctrl ctx oaddr queue action)
-      superviseWith (fmap not $ closed ctrl) name (routingLoop ifh ofh queue)
+      superviseWith (fmap not $ closed ctrl) name (routingLoop ifh ofh)
 
     where
       oaddr = printf "inproc://%s.hzmq-router" name
        
-      procRequest fh queue = do
+      procRequest fh = do
         mreq <- recvRequest fh
-        when (isJust mreq) (request queue (fromJust mreq))
+        when (isJust mreq) (forkWorker ctx oaddr (fromJust mreq) action)
        
-      routingLoop :: Socket Router -> Socket Pull -> Device Request -> IO ()
-      routingLoop ifh ofh queue = do
-        [eifh, eofh] <- poll (-1) [Sock ifh [In] Nothing, Sock ofh [In] Nothing]
-        when (not $ null eifh) (procRequest ifh queue)
+      routingLoop :: Socket Router -> Socket Pull -> IO ()
+      routingLoop ifh ofh = do
+        [eifh, eofh] <- poll 1000 [Sock ifh [In] Nothing, Sock ofh [In] Nothing]
+        when (not $ null eifh) (procRequest ifh)
         when (not $ null eofh) (fmap fromList (receiveMulti ofh) >>= sendMulti ifh)
 
