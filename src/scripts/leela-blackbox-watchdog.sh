b9191b1f7addb9ffae45d8e7ca11afd6d3ce469e
dgvncsz0f <dsouza@c0d3.xxx>
2013-12-05 21:58:45 -0200

    bugfix: using correct default file: /etc/default/leela-blackbox

--- src/scripts/leela-blackbox-watchdog.sh
+++ src/scripts/leela-blackbox-watchdog.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-. /etc/default/leela/blackbox
+. /etc/default/leela-blackbox
 
 echo "$LEELA_BLACKBOX_ENDPOINT" | cut -c 7- | { IFS=:; read host port &&
   nc -z $host $port && echo "tcp://$host:$port"
