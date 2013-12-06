324d214626fde8616f4e39e9a91aa533523ba336
dgvncsz0f <dsouza@c0d3.xxx>
2013-12-06 00:48:24 -0200

    monitors warpdrive

---
+++ src/scripts/leela-warpdrive-watchdog.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+. /etc/default/leela-warpdrive
+
+echo "$LEELA_WARPDRIVE_ENDPOINT" | cut -c 7- | { IFS=:; read host port &&
+  nc -z $host $port && echo "tcp://$host:$port"
+}
