2f08b00dc15af4d17c74b2fd636b35ab8039b26a
dgvncsz0f <dsouza@c0d3.xxx>
2013-12-06 01:31:31 -0200

    adding more information: hostname & uptime

--- src/scripts/leela-warpdrive-watchdog.sh
+++ src/scripts/leela-warpdrive-watchdog.sh
@@ -5,5 +5,9 @@ set -e
 . /etc/default/leela-warpdrive
 
 echo "$LEELA_WARPDRIVE_ENDPOINT" | cut -c 7- | { IFS=:; read host port &&
-  nc -z $host $port && echo "tcp://$host:$port"
+  nc -z $host $port && {
+    echo -n "hostname > "; hostname
+    echo -n "uptime   > "; uptime
+    echo "tcp://$host:$port"
+  }
 }
