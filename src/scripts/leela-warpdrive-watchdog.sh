d8a9b62eb595598bb7c990ea8b9a64615033efdd
dgvncsz0f <dsouza@c0d3.xxx>
2013-12-09 14:53:27 -0200

    endpoints must terminate with ; (semi-colon)

--- src/scripts/leela-warpdrive-watchdog.sh
+++ src/scripts/leela-warpdrive-watchdog.sh
@@ -7,6 +7,6 @@ set -e
 echo "$LEELA_WARPDRIVE_ENDPOINT" | cut -c 7- | { IFS=:; read host port &&
   nc -z $host $port && {
     echo -n "-- hostname: "; hostname
-    echo "tcp://$host:$port"
+    echo "tcp://$host:$port;"
   }
 }
