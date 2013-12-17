e97eca4348f502d8aceb5271c87b8203a0879d46
dgvncsz0f <dsouza@c0d3.xxx>
2013-12-17 09:48:33 -0200

    bugfix: linking against libleela only (no version)

--- src/python/setup.py
+++ src/python/setup.py
@@ -3,31 +3,29 @@
 from distutils.core import setup
 from distutils.core import Extension
 
-version = "4.0.0"
-
 ffi_endpoint = Extension("_leela_endpoint",
                          sources             = ["src/ffi/python_endpoint.c"],
                          include_dirs        = ["../c/src"],
-                         libraries           = ["leela%s" % version],
+                         libraries           = ["leela"],
                          library_dirs        = ["../c"],
                          extra_compile_args  = ["-std=c99", "-Wall"])
 
 ffi_naming = Extension("_leela_naming",
                        sources             = ["src/ffi/python_naming.c"],
                        include_dirs        = ["../c/src"],
-                       libraries           = ["leela%s" % version],
+                       libraries           = ["leela"],
                        library_dirs        = ["../c"],
                        extra_compile_args  = ["-std=c99", "-Wall"])
 
 ffi_lql = Extension("_leela_lql",
                     sources             = ["src/ffi/python_lql.c"],
                     include_dirs        = ["../c/src"],
-                    libraries           = ["leela%s" % version],
+                    libraries           = ["leela"],
                     library_dirs        = ["../c"],
                     extra_compile_args  = ["-std=c99", "-Wall"])
 
 setup(name="leela",
-      version      = version,
+      version      = "4.0.0",
       license      = "APACHE-2",
       description  = "Leela - scalable metrics monitoring engine",
       author       = "Diego Souza",
