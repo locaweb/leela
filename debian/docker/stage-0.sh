2bf874d02954fa1e676d9b2bd7255514dd143568
dgvncsz0f <dsouza@c0d3.xxx>
2013-12-16 23:57:28 -0200

    creating wheezy and squeeze build images

--- debian/docker/stage-0.sh
+++ debian/docker/stage-0.sh
@@ -5,8 +5,13 @@ set -x
 target=leela-stage0-$RANDOM
 pkglist=iproute,iputils-ping,netbase
 
-rm -rf "$target"
-mkdir -p "$target"
-debootstrap --verbose --variant=buildd --include=$pkglist wheezy "$target" http://cdn.debian.net/debian
-tar -C "$target" -c . | docker import - leela/stage0
-rm -rf "$target"
+make_target() {
+  rm -rf "$target"
+  mkdir -p "$target"
+  debootstrap --verbose --variant=buildd --include=$pkglist $1 "$target" http://cdn.debian.net/debian
+  tar -C "$target" -c . | docker import - leela/stage0-$1
+  rm -rf "$target"
+}
+
+make_target wheezy
+make_target squeeze
