a7c1b3ec0e3872aca3547b01495c8ea8a5b0ea6f
PotHix <pothix@pothix.com>
2014-01-17 19:47:10 -0200

    Fixed copyright date.

--- src/python/src/ffi/python_lql.c
+++ src/python/src/ffi/python_lql.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011, Diego Souza                                                 */
+/* Copyright (c) 2013, Diego Souza                                                 */
 /* All rights reserved.                                                            */
                                                                                    
 /* Redistribution and use in source and binary forms, with or without              */
