744d48278a63a1dff4c5c8b96445964f625a0f49
dgvncsz0f <dsouza@c0d3.xxx>
2014-01-30 20:22:27 -0200

    improvind tests a bit, nothing important

--- try/src/try_leela/suites/fast/test_kill.py
+++ try/src/try_leela/suites/fast/test_kill.py
@@ -15,10 +15,10 @@ class TestKill(unittest.TestCase):
             b_guid = helpers.make(session)
             c_guid = helpers.make(session)
             helpers.link(session, a_guid, "a", b_guid)
-            helpers.link(session, a_guid, "b", c_guid)
+            helpers.link(session, a_guid, "c", c_guid)
             self.assertEqual(2, session.execute_fmap(len, "path %s -[*]> ()" % (a_guid,)))
-            helpers.kill(session, a_guid, "a", b_guid)
-            self.assertEqual([["path", [["b", c_guid]]]], session.execute_fetch("path %s -[*]> ()" % (a_guid,)))
+            helpers.kill(session, a_guid, "a")
+            self.assertEqual([["path", [["c", c_guid]]]], session.execute_fetch("path %s -[*]> ()" % (a_guid,)))
 
     def test_kill_with_right_node(self):
         with self.driver.session("fast/test_kill") as session:
