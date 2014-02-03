1cb68aea8c1e21c6539c9439679277c6604bb558
PotHix <pothix@pothix.com>
2014-02-03 16:45:42 -0200

    Added a simple script to run integration tests for ruby.

--- src/ruby/leela_ruby/spec/leela_ruby/connection_spec.rb
+++ src/ruby/leela_ruby/spec/leela_ruby/connection_spec.rb
@@ -6,4 +6,6 @@ describe Leela::Connection do
       Leela::Connection.new("tcp://warp9999.locaweb.com.br:4080", "pothix", "V1fR0sTo")
     }.to raise_error(Leela::LeelaError)
   end
+
+  it "also run all the integration tests by using the spec/run scrips passing the endpoint as argument"
 end
