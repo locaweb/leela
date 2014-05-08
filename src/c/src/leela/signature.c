4a7948c0f5f20a8d2bf3a5b616b519561101bd56
dgvncsz0f <dsouza@c0d3.xxx>
2014-05-08 07:03:41 -0300

    poly1305aes [1] signature module
    [1] http://cr.yp.to/mac.html

---
+++ src/c/src/leela/signature.c
@@ -0,0 +1,89 @@
+// Copyright 2014 (c) Diego Souza <dsouza@c0d3.xxx>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include "signature.h"
+#include "poly1305aes/poly1305aes.h"
+
+struct leela_signature_t
+{
+  unsigned char seed[LEELA_SIGNATURE_SEED_SIZE];
+};
+
+static inline
+char __henc(unsigned char i)
+{
+  return(i < 10 ? (i + 48) : (i+87));
+}
+
+static inline
+unsigned char __hdec(unsigned char i)
+{
+  return(i > 87 ? (i - 87) : (i - 48));
+}
+
+void leela_signature_hexencode (char *dst, const unsigned char *src, size_t size)
+{
+  size_t i;
+  for (i=0; i<size; i+=1)
+  {
+    dst[2 * i]     = __henc(src[i] >> 4);
+    dst[2 * i + 1] = __henc(src[i] & 0x0F);
+  }
+  dst[2 * size] = '\0';
+}
+
+void leela_signature_hexdecode (unsigned char *dst, size_t size, const char *src)
+{
+  size_t i;
+  for (i=0; i<size; i+=1)
+  { dst[i] = (__hdec(src[2 * i]) << 4) | __hdec(src[2 * i + 1]); }
+}
+
+leela_signature_t *leela_signature_init (unsigned char seed[LEELA_SIGNATURE_SEED_SIZE])
+{
+  leela_signature_t *sig = (leela_signature_t *) malloc(sizeof(leela_signature_t));
+  if (sig != NULL)
+  {
+    memcpy(sig->seed, seed, LEELA_SIGNATURE_SEED_SIZE);
+    poly1305aes_clamp(sig->seed);
+  }
+  return(sig);
+}
+
+void leela_signature_nonce_next (unsigned char dst[LEELA_SIGNATURE_NONCE_SIZE], const unsigned char src[LEELA_SIGNATURE_NONCE_SIZE])
+{
+  uint64_t nonce[2] = {0, 0};
+  memcpy(nonce, src, LEELA_SIGNATURE_NONCE_SIZE);
+
+  nonce[0] += 1;
+  if (nonce[0] < 1)
+  { nonce[1] += 1; }
+
+  memcpy(dst, nonce, LEELA_SIGNATURE_NONCE_SIZE);
+}
+
+void leela_signature_sign (leela_signature_t *sig, unsigned char mac[LEELA_SIGNATURE_SIZE], const unsigned char nonce[LEELA_SIGNATURE_SIZE], const void *msg, unsigned int msglen)
+{ poly1305aes_authenticate(mac, sig->seed, nonce, (unsigned char *) msg, msglen); }
+
+int leela_signature_check (leela_signature_t *s, const unsigned char sig[LEELA_SIGNATURE_SIZE], const unsigned char nonce[LEELA_SIGNATURE_SIZE], const unsigned char *msg, unsigned int msglen)
+{
+  return(poly1305aes_verify(sig, s->seed, nonce, msg, msglen) != 0 ? 0 : -1);
+}
+
+void leela_signature_destroy (leela_signature_t *sig)
+{ free(sig); }
---
+++ src/c/src/leela/signature.h
@@ -0,0 +1,102 @@
+// Copyright 2014 (c) Diego Souza <dsouza@c0d3.xxx>
+//  
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//  
+//     http://www.apache.org/licenses/LICENSE-2.0
+//  
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef __leela_signature_h__
+#define __leela_signature_h__
+
+#include <stdlib.h>
+#include "base.h"
+
+LEELA_CPLUSPLUS_OPEN
+
+#define LEELA_SIGNATURE_SIZE 16
+#define LEELA_SIGNATURE_HEX_SIZE 33
+#define LEELA_SIGNATURE_SEED_SIZE 32
+#define LEELA_SIGNATURE_NONCE_SIZE 16
+
+typedef struct leela_signature_t leela_signature_t;
+
+/*! Encodes the signature or nonce using base16 into a valid c-string
+ *  (null terminated).
+ *
+ *  \param dst A variable that can take at least size*2+1 bytes;
+ *  
+ *  \param src The buffer to encode;
+ *  
+ *  \param size The size of src;
+ */
+void leela_signature_hexencode (char *dst, const unsigned char *src, size_t size);
+
+/*! Decodes a hex-encoded string.
+ *
+ *  \param dst A variable that can take size bytes;
+ *  
+ *  \param src The buffer to decode (this must be at least size*2+1 bytes);
+ *  
+ *  \param size The size of dst;
+ */
+void leela_signature_hexdecode (unsigned char *dst, size_t size, const char *src);
+
+/*! Initializes the signature framework. The seed must be exactly
+ *  LEELA_SIGNATURE_SEED_SIZE long.
+ *
+ *  \return NULL: something went wrong;
+ *          _   : a valid pointer;
+ */
+leela_signature_t *leela_signature_init (unsigned char seed[LEELA_SIGNATURE_SEED_SIZE]);
+
+/*! Generates a new nonce.
+ */
+void leela_signature_nonce_next (unsigned char dst[LEELA_SIGNATURE_NONCE_SIZE], const unsigned char src[LEELA_SIGNATURE_NONCE_SIZE]);
+
+/*! Signs a new message.
+ *
+ *  \param sig The pointer created with leela_signature_init;
+ *  
+ *  \param mac The variable that takes the signature;
+ *  
+ *  \param nonce The nonce to use. Make sure you use the
+ *  leela_signature_nonce to generate a new nonce (extremely
+ *  important!);
+ *  
+ *  \param msg The message to sign;
+ *  
+ *  \param msglen The size of this message;
+ */
+void leela_signature_sign (leela_signature_t *sig, unsigned char mac[LEELA_SIGNATURE_SIZE], const unsigned char nonce[LEELA_SIGNATURE_NONCE_SIZE], const void *msg, unsigned int msglen);
+
+/*! Checks wheter or not the signature is valid.
+ *
+ *  \param s The pointer created with leela_signature_init;
+ *  
+ *  \param sig The signature to check;
+ *  
+ *  \param nonce The nonce used to sign this message;
+ *  
+ *  \param msg The message to check;
+ *  
+ *  \param msglen The size of this message;
+ *  
+ *  \return 0: Ok;
+ *          x: An error has ocurred;
+ */
+int leela_signature_check (leela_signature_t *s, const unsigned char sig[LEELA_SIGNATURE_SIZE], const unsigned char nonce[LEELA_SIGNATURE_NONCE_SIZE], const unsigned char *msg, unsigned int msglen);
+
+/*! Frees all memory.
+ */
+void leela_signature_destroy (leela_signature_t *sig);
+
+LEELA_CPLUSPLUS_CLOSE
+
+#endif
