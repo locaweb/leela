6a7bbf5fbd8d4113ff60b31857ccb516b7a3fa38
Stephano Ferreira <stephano.ferreira@locaweb.com.br>
2014-08-15 15:00:43 -0300

    Add main task for build-native.

---
+++ automation/ansible/roles/build-native/files/build-warpdrive.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+basedir=$(dirname $0)
+
+# UPDATE CABAL TO THE LATEST VERSION
+cabal update; cabal install cabal cabal-install
+
+# CREATE A LINK FOR THE UPDATED CABAL
+mv /usr/bin/cabal /usr/bin/cabal.old; ln -s ~/.cabal/bin/cabal /usr/bin/cabal
+
+# BUILD WARPDRIVE
+cd ${basedir}/leela/src/warpdrive; cabal update; cabal sanbox init; \
+  cabal install --only-dependencies; cabal configure -O2; cabal build
