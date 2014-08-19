14e1fe9f5fde6bde93240e01f61921ae74a6af50
Stephano Ferreira <stephano.ferreira@locaweb.com.br>
2014-08-19 15:50:33 -0300

    Remove cabal update and installation from build-warpdrive.sh

--- automation/ansible/roles/build-native/files/build-warpdrive.sh
+++ automation/ansible/roles/build-native/files/build-warpdrive.sh
@@ -2,12 +2,6 @@
 
 basedir=$(dirname $0)
 
-# UPDATE CABAL TO THE LATEST VERSION
-cabal update; cabal install cabal cabal-install
-
-# CREATE A LINK FOR THE UPDATED CABAL
-mv /usr/bin/cabal /usr/bin/cabal.old; ln -s ~/.cabal/bin/cabal /usr/bin/cabal
-
 # BUILD WARPDRIVE
-cd ${basedir}/leela/src/warpdrive; cabal update; cabal sanbox init; \
+cd ${basedir}/leela/src/warpdrive; cabal update; cabal sandbox init; \
   cabal install --only-dependencies --force-reinstalls; cabal configure -O2; cabal build
