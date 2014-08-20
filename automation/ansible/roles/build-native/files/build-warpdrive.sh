52808a3c7484da48032548aaee71d187100cf766
Stephano Ferreira <stephano.ferreira@locaweb.com.br>
2014-08-20 15:09:18 -0300

    Improve build-warpdrive.sh.

--- automation/ansible/roles/build-native/files/build-warpdrive.sh
+++ automation/ansible/roles/build-native/files/build-warpdrive.sh
@@ -2,6 +2,10 @@
 
 basedir=$(dirname $0)
 
+if [ -e /opt/zmq4/lib/pkgconfig ]; then
+  export PKG_CONFIG_PATH=/opt/zmq4/lib/pkgconfig
+fi
+
 # BUILD WARPDRIVE
-cd ${basedir}/leela/src/warpdrive; cabal update; cabal sandbox init; \
+cd ${basedir}/leela/src/warpdrive; rm -r dist; cabal update; cabal sandbox init; \
   cabal install --only-dependencies --force-reinstalls; cabal configure -O2; cabal build
