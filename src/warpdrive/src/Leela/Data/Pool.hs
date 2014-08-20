c26508e84b041de4fe566f8e9f9dd3166a81c4e8
dgvncsz0f <dsouza@c0d3.xxx>
2014-08-20 18:30:39 -0300

    add round-robin policy strategy for pool resource usage

--- src/warpdrive/src/Leela/Data/Pool.hs
+++ src/warpdrive/src/Leela/Data/Pool.hs
@@ -19,20 +19,21 @@ module Leela.Data.Pool
        , createPool
        , updatePool
        , deletePool
+       , rr
        , use
        , useAll
        ) where
 
 import qualified Data.Set as S
-import           Data.Maybe
 import           Control.Monad
 import qualified Data.Map.Strict as M
-import           Control.Exception (bracket)
+import           Control.Exception (bracket, throwIO)
 import           Leela.Data.Excepts
 import           Control.Concurrent.STM
 
 data Pool a b = Pool { state  :: TVar (M.Map a b)
                      , using  :: TVar (M.Map a Int)
+                     , rridx  :: TVar (Int, Int)
                      , create :: a -> IO b
                      , delete :: a -> b -> IO ()
                      }
@@ -41,7 +42,8 @@ createPool :: (a -> IO b) -> (a -> b -> IO ()) -> IO (Pool a b)
 createPool add del = do
   m0 <- newTVarIO M.empty
   m1 <- newTVarIO M.empty
-  return $ Pool m0 m1 add del
+  m2 <- newTVarIO (0, 0)
+  return $ Pool m0 m1 m2 add del
 
 updatePool :: (Ord a) => Pool a b -> [a] -> IO ()
 updatePool pool lcluster = do
@@ -57,15 +59,22 @@ deletePool pool = do
   items <- atomically $ do
     m <- readTVar (state pool)
     writeTVar (state pool) M.empty
+    writeTVar (rridx pool) (0, 0)
     return (M.toList m)
   mapM_ (uncurry $ delete pool) items
 
+updateState :: Pool a b -> (M.Map a b -> (c, M.Map a b)) -> STM c
+updateState pool f = do
+  (c, m) <- fmap f (readTVar $ state pool)
+  let sz = M.size m
+  writeTVar (state pool) m
+  modifyTVar' (rridx pool) (\(ix, _) -> (ix, sz))
+  return c
+
 kill :: (Ord a) => Pool a b -> a -> IO ()
 kill pool k = do
-  mhandle <- atomically $ do
-    m0 <- readTVar (state pool)
-    writeTVar (state pool) (M.delete k m0)
-    return (M.lookup k m0)
+  mhandle <- atomically $
+    updateState pool (\m -> (M.lookup k m, M.delete k m))
   _       <- atomically $ do
     m1 <- readTVar (using pool)
     when (M.findWithDefault 0 k m1 /= 0) retry
@@ -76,19 +85,38 @@ kill pool k = do
 
 open :: (Ord a) => Pool a b -> a -> IO ()
 open pool k = do
-  handle  <- create pool k
-  atomically $ modifyTVar (state pool) (M.insert k handle)
+  handle <- create pool k
+  atomically $
+    updateState pool (\m -> ((), M.insert k handle m))
 
 use :: (Ord a) => Pool a b -> ([a] -> a) -> (b -> IO c) -> IO c
 use pool select apply = bracket acquire release (apply . snd)
     where
-      acquire = atomically $ do
-        m <- readTVar (state pool)
+      acquire = do
+        m <- atomically $ readTVar (state pool)
         let a = select (M.keys m)
-        modifyTVar' (using pool) (M.insertWith (+) a 1)
         case (M.lookup a m) of
-          Just b -> return (a, b)
-          _      -> throwSTM (SystemExcept (Just "Pool/use: can't find pool entry"))
+          Just b -> atomically $ do
+            modifyTVar' (using pool) (M.insertWith (+) a 1)
+            return (a, b)
+          _      -> throwIO (SystemExcept (Just "Pool/use: can't find pool entry"))
+
+      release (a, _) = atomically $
+        modifyTVar' (using pool) (M.insertWith (-) a 1)
+
+rr :: (Ord a) => Pool a b -> (b -> IO c) -> IO c
+rr pool apply = bracket acquire release (apply . snd)
+    where
+      acquire = do
+        (at, m) <- atomically $ do
+          (ix, sz) <- readTVar $ rridx pool
+          let next = (ix + 1) `mod` sz
+          writeTVar (rridx pool) (next, sz)
+          fmap (next, ) (readTVar $ state pool)
+        let (a, b) = M.elemAt at m
+        atomically $ do
+          modifyTVar' (using pool) (M.insertWith (+) a 1)
+          return (a, b)
 
       release (a, _) = atomically $
         modifyTVar' (using pool) (M.insertWith (-) a 1)
@@ -96,11 +124,12 @@ use pool select apply = bracket acquire release (apply . snd)
 useAll :: (Ord a) => Pool a b -> ([b] -> IO c) -> IO c
 useAll pool apply = bracket acquire release (apply . snd)
     where
-      acquire = atomically $ do
-        m <- readTVar (state pool)
-        let as = M.keys m
-        mapM_ (\a -> modifyTVar' (using pool) (M.insertWith (+) a 1)) as
-        return (as, mapMaybe (flip M.lookup m) as)
+      acquire = do
+        m <- atomically $ readTVar (state pool)
+        let (as, vs) = unzip (M.assocs m)
+        atomically $ do
+          mapM_ (\a -> modifyTVar' (using pool) (M.insertWith (+) a 1)) as
+          return (as, vs)
 
       release (as, _) = atomically $
         mapM_ (\a -> modifyTVar' (using pool) (M.insertWith (-) a 1)) as
