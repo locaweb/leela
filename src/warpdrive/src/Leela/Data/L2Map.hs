f9808993750042bf71095e37f2fc18456c5f1f9d
dgvncsz0f <dsouza@c0d3.xxx>
2014-10-02 15:20:27 -0300

    using stm-containers instead of IORef HashMap

--- src/warpdrive/src/Leela/Data/L2Map.hs
+++ src/warpdrive/src/Leela/Data/L2Map.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TupleSections #-}
+
 -- Copyright 2014 (c) Diego Souza <dsouza@c0d3.xxx>
 --
 -- Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,52 +20,30 @@ module Leela.Data.L2Map
        , insert
        , lookup
        , delete
-       , toList
        ) where
 
+import qualified Focus as F
 import           Prelude hiding (lookup)
 import           Data.Hashable
-import qualified Data.HashMap.Strict as M
+import qualified STMContainers.Map as M
 import           Control.Concurrent.STM
 
-newtype L2Map k1 k2 v = L2Map (TVar (M.HashMap k1 (TVar (M.HashMap k2 v))))
+newtype L2Map k1 k2 v = L2Map (M.Map (k1, k2) v)
 
 empty :: IO (L2Map k1 k2 v)
-empty = fmap L2Map (newTVarIO M.empty)
+empty = fmap L2Map (atomically M.new)
 
 insert :: (Eq k1, Eq k2, Hashable k1, Hashable k2) => k1 -> k2 -> v -> L2Map k1 k2 v -> IO ()
-insert k1 k2 value (L2Map tm0) = do
-  tm1 <- atomically $ do
-    m <- readTVar tm0
-    v <- newTVar M.empty
-    case (M.lookup k1 m) of
-      Nothing -> writeTVar tm0 (M.insert k1 v m) >> return v
-      Just v1 -> return v1
-  atomically $ do
-    m <- readTVar tm1
-    writeTVar tm1 (M.insert k2 value m)
+insert k1 k2 value (L2Map m) =
+  let k = (k1, k2)
+  in atomically $ M.insert value k m
 
 lookup :: (Eq k1, Eq k2, Hashable k1, Hashable k2) => k1 -> k2 -> L2Map k1 k2 v -> IO (Maybe v)
-lookup k1 k2 (L2Map tm0) = do
-  mtm1 <- fmap (M.lookup k1) (readTVarIO tm0)
-  case mtm1 of
-    Nothing  -> return Nothing
-    Just tm1 -> fmap (M.lookup k2) (readTVarIO tm1)
+lookup k1 k2 (L2Map m) =
+  let k = (k1, k2)
+  in atomically $ M.lookup k m
 
 delete :: (Eq k1, Eq k2, Hashable k1, Hashable k2) => k1 -> k2 -> L2Map k1 k2 v -> IO (Maybe v)
-delete k1 k2 (L2Map tm0) = atomically $ do
-  m0 <- readTVar tm0
-  case (M.lookup k1 m0) of
-    Nothing  -> return Nothing
-    Just tm1 -> do
-      (a, m1) <- fmap (\m -> (M.lookup k2 m, M.delete k2 m)) (readTVar tm1)
-      writeTVar tm1 m1
-      return a
-
-toList :: L2Map k1 k2 v -> ([((k1, k2), v)] -> b -> IO b) -> b -> IO b
-toList (L2Map tm0) action b = readTVarIO tm0 >>= go b . M.toList
-  where go acc []            = return acc
-        go acc ((k, v) : xs) = do
-          vs <- fmap (map (\(k1, v1) -> ((k, k1), v1)) . M.toList) (readTVarIO v)
-          flip go xs =<< action vs acc
-          
+delete k1 k2 (L2Map m) =
+  let k = (k1, k2)
+  in atomically $ M.focus (\r -> return (r, F.Remove)) k m
