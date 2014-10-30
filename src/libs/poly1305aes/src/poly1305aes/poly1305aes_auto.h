a49b93247007a7cd3aa5f2f399b1f9a918c18a88
dgvncsz0f <dsouza@c0d3.xxx>
2014-10-30 18:39:30 -0200

    bugfix: use poly1305 files do dectect architecture and fallback to compiler macro only when these are missing

--- src/libs/poly1305aes/src/poly1305aes/poly1305aes_auto.h
+++ src/libs/poly1305aes/src/poly1305aes/poly1305aes_auto.h
@@ -1,7 +1,7 @@
 #ifndef poly1305aes_auto__
 #define poly1305aes_auto__
 
-#if defined(__x86_64__) || defined(_WIN64)
+#if defined(POLY_USE_53) || ((!defined(POLY_USE_ppro) && !defined(POLY_USE_athlon)) && (defined(__x86_64__) || defined(_WIN64)))
 
 #ifdef __cplusplus
 extern "C" {
@@ -32,7 +32,7 @@ extern "C" {
 # define poly1305aes_auto_poly1305aes_authenticate_c  "poly1305aes-20050218/poly1305aes_53_authenticate.c"
 # define poly1305aes_auto_poly1305aes_authenticate_s  "empty.c"
 
-#elif defined(__i386__) || defined(_WIN32)
+#elif defined(POLY_USE_athlon)
 
 # include "poly1305aes-20050218/aes_athlon.h"
 # include "poly1305aes-20050218/poly1305_athlon.h"
@@ -53,6 +53,29 @@ extern "C" {
 # define poly1305aes_auto_poly1305aes_isequal_c      "empty.c"
 # define poly1305aes_auto_poly1305aes_isequal_s      "poly1305aes-20050218/poly1305aes_athlon_isequal.s"
 # define poly1305aes_auto_poly1305aes_authenticate_c "poly1305aes-20050218/poly1305aes_athlon_authenticate.c"
+# define poly1305aes_auto_poly1305aes_authenticate_s "empty.c"
+
+#elif defined(POLY_USE_ppro) || ((!defined(POLY_USE_53) && !defined(POLY_USE_athlon)) && (defined(__i386__) || defined(_WIN32)))
+
+# include "poly1305aes-20050218/aes_ppro.h"
+# include "poly1305aes-20050218/poly1305_ppro.h"
+# include "poly1305aes-20050218/poly1305aes_ppro.h"
+
+# define poly1305aes_auto_aes_c                      "empty.c"
+# define poly1305aes_auto_aes_s                      "poly1305aes-20050218/aes_ppro.s"
+# define poly1305aes_auto_poly1305_c                 "empty.c"
+# define poly1305aes_auto_poly1305_s                 "poly1305aes-20050218/poly1305_ppro.s"
+# define poly1305aes_auto_aes_constants_c            "empty.c"
+# define poly1305aes_auto_aes_constants_s            "poly1305aes-20050218/aes_ppro_constants.s"
+# define poly1305aes_auto_poly1305aes_clamp_s        "empty.c"
+# define poly1305aes_auto_poly1305aes_clamp_c        "poly1305aes-20050218/poly1305aes_ppro_clamp.c"
+# define poly1305aes_auto_poly1305aes_verify_s       "empty.c"
+# define poly1305aes_auto_poly1305aes_verify_c       "poly1305aes-20050218/poly1305aes_ppro_verify.c"
+# define poly1305aes_auto_poly1305_constants_c       "empty.c"
+# define poly1305aes_auto_poly1305_constants_s       "poly1305aes-20050218/poly1305_ppro_constants.s"
+# define poly1305aes_auto_poly1305aes_isequal_c      "empty.c"
+# define poly1305aes_auto_poly1305aes_isequal_s      "poly1305aes-20050218/poly1305aes_ppro_isequal.s"
+# define poly1305aes_auto_poly1305aes_authenticate_c "poly1305aes-20050218/poly1305aes_ppro_authenticate.c"
 # define poly1305aes_auto_poly1305aes_authenticate_s "empty.c"
 
 #endif
