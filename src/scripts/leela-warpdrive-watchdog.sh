bce7ac2a61ccb8cb6c24635df0fc0f1f53ffaeba
dgvncsz0f <dsouza@c0d3.xxx>
2014-11-28 13:25:15 -0200

    failfast: watchdog now tries to connect to the cluster using the local endpoint

--- src/scripts/leela-warpdrive-watchdog.sh
+++ src/scripts/leela-warpdrive-watchdog.sh
@@ -2,13 +2,25 @@
 
 . /etc/default/leela-warpdrive
 
-echo "$LEELA_WARPDRIVE_ENDPOINT" | cut -c 7- | { IFS=:; read host port &&
-  nc -z $host $port
-}
+env LEELA_WARPDRIVE_WATCHDOG_USER=$LEELA_WARPDRIVE_WATCHDOG_USER \
+    LEELA_WARPDRIVE_WATCHDOG_PASS=$LEELA_WARPDRIVE_WATCHDOG_PASS \
+    LEELA_WARPDRIVE_ENDPOINT=$LEELA_WARPDRIVE_ENDPOINT python2.7 <<EOF
+import os
+import sys
+import time
+from pyleela import lql
 
-if [ "$?" -eq 0 ]
-then
-  exit 0
-else
-  exit 2
-fi
+user     = os.environ["LEELA_WARPDRIVE_WATCHDOG_USER"]
+secret   = os.environ["LEELA_WARPDRIVE_WATCHDOG_PASS"]
+cluster  = [os.environ["LEELA_WARPDRIVE_ENDPOINT"]]
+
+def health_check ():
+  for at in range(5):
+    try:
+      with lql.with_context(cluster, user, secret, 10000): pass
+      return(True)
+    except: time.sleep(1 << at)
+
+ok = health_check()
+sys.exit(0 if ok else 1)
+EOF
