35650e8ab3f92c0a315b9edf1052a9b1efa7db5f
Andre Ferraz <andre.ferraz@locaweb.com.br>
2015-01-21 14:49:46 -0200

    cassaforte upgrade to 2.0.0 release

--- src/blackbox/project.clj
+++ src/blackbox/project.clj
@@ -11,7 +11,7 @@
                  [clj-aws-s3 "0.3.10"]
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.cli "0.2.4"]
-                 [clojurewerkz/cassaforte "2.0.0-rc1"]
+                 [clojurewerkz/cassaforte "2.0.0"]
                  [org.clojure/tools.logging "0.2.6"]
                  [org.apache.cassandra/cassandra-all "2.1.2"]]
 )
