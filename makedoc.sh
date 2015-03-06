67eed46e2950224fc3fed42e7e7b3e192b767c16
dgvncsz0f <dsouza@c0d3.xxx>
2015-03-06 16:28:06 -0300

    rename site/ to doc/

--- makedoc.sh
+++ makedoc.sh
@@ -6,7 +6,7 @@ buildroot=$(mktemp -d) && {
   trap "rm -rf \"$buildroot\"" INT QUIT TERM EXIT
   git archive --format=tar --prefix=leela/ ${branch:-master} | tar -C "$buildroot" -x
   (cd "$buildroot/leela/doc" && "${mkdocs_bin:-mkdocs}" build)
-  rm -rf site
-  mv "$buildroot/leela/doc/site/" site/
+  rm -rf doc
+  mv "$buildroot/leela/doc/site/" doc/
   rm -rf "$buildroot"
 }
