f67f4b4a982b002166dc6cd769d5402f1968c0d7
dgvncsz0f <dsouza@c0d3.xxx>
2015-03-06 16:20:13 -0300

    leela documentation

--- makedoc.sh
+++ makedoc.sh
@@ -6,7 +6,7 @@ buildroot=$(mktemp -d) && {
   trap "rm -rf \"$buildroot\"" INT QUIT TERM EXIT
   git archive --format=tar --prefix=leela/ ${branch:-master} | tar -C "$buildroot" -x
   (cd "$buildroot/leela/doc" && "${mkdocs_bin:-mkdocs}" build)
-  ls -l "$buildroot/leela/doc/site"
-  cp -Ta "$buildroot/leela/doc/site/" .
+  rm -rf site
+  mv "$buildroot/leela/doc/site/" site/
   rm -rf "$buildroot"
 }
