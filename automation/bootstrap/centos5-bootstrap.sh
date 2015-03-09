fe358da138e53f556abe0f762708c27d8b6f9163
dgvncsz0f <dsouza@c0d3.xxx>
2015-03-09 20:03:37 -0300

    no longer fails if user already exists

--- automation/bootstrap/centos5-bootstrap.sh
+++ automation/bootstrap/centos5-bootstrap.sh
@@ -6,7 +6,7 @@ srcroot=${srcroot:-$(dirname $(readlink -f "$0"))}
 
 . "$srcroot/bootstrap-lib.sh"
 
-adduser --home-dir /home/leela --shell /bin/sh leela
+adduser --home-dir /home/leela --shell /bin/sh leela || true
 
 rpm_install wget
 if [ "$arch" = "i386" ]
@@ -14,4 +14,4 @@ then rpm_install_url epel-release http://download.fedoraproject.org/pub/epel/5/i
 elif [ "$arch" = "amd64" ]
 then rpm_install_url epel-release http://download.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
 fi
-rpm_install cmake28 git rpmdevtools pkgconfig
+rpm_install cmake28 git rpmdevtools rpm-build pkgconfig
--- automation/bootstrap/centos6-bootstrap.sh
+++ automation/bootstrap/centos6-bootstrap.sh
@@ -6,6 +6,6 @@ srcroot=${srcroot:-$(dirname $(readlink -f "$0"))}
 
 . "$srcroot/bootstrap-lib.sh"
 
-adduser --home-dir /home/leela --shell /bin/sh leela
+adduser --home-dir /home/leela --shell /bin/sh leela || true
 
-rpm_install wget cmake git rpmdevtools pkgconfig
+rpm_install wget cmake git rpmdevtools rpm-build pkgconfig
