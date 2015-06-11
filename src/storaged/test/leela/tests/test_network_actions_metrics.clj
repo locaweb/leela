664cf074b61e5c4c935df278b3a781b8a8e5f489
dgvncsz0f <dsouza@c0d3.xxx>
2015-06-11 07:27:46 -0300

    bugfix: pagination test

--- src/storaged/test/leela/tests/test_network_actions_metrics.clj
+++ src/storaged/test/leela/tests/test_network_actions_metrics.clj
@@ -10,18 +10,18 @@
 (install-keyspace-fixtures [metrics/create-schema])
 
 (deftest test-get-metric-handler-empty
-  (is (empty? (get-metrics-handler {:schema 0
+  (is (empty? (get-metrics-handler {:plane 0
                                     :metric 0
                                     :bucket 0
                                     :offset 0}))))
 
 (deftest test-get-metric-after-put-metric
-  (let [data  {:schema (rand-int 1000)
+  (let [data  {:plane (rand-int 1000)
                :metric (rand-int 1000)
                :bucket (rand-int 1000)
                :offset (rand-int 1000)
                :datum (bytes/bytes-from-chars "foobar")}
-        query {:schema (:schema data)
+        query {:plane (:plane data)
                :metric (:metric data)
                :bucket (:bucket data)}]
     (put-metrics-handler data)
@@ -30,15 +30,14 @@
       (is (= (:offset data) (:offset (first rows))))
       (is (= (seq (:datum data)) (seq (:datum (first rows))))))))
 
-;; (deftest-pagination-asc test-worker-pagination-on-metrics
-;;   (let [data  {:schema (rand-int 1000)
-;;                :metric (rand-int 1000)
-;;                :bucket (rand-int 1000)
-;;                :datum (bytes/bytes-from-chars "foobar")}
-;;         query {:schema (:schema data)
-;;                :metric (:metric data)
-;;                :bucket (:metric data)}]
-;;     {:store-fn #(put-metrics-handler (assoc data :offset %1))
-;;      :fetch-fn #(get-metrics-handler (assoc query :offset %1 :limit %2))
-;;      :mkrow-fn #(identity {:offset %})
-;;      :view-fn  #(:offset %)}))
+(deftest-pagination-asc test-worker-pagination-on-metrics
+  (let [data  {:plane (rand-int 1000)
+               :metric (rand-int 1000)
+               :bucket (rand-int 1000)
+               :datum (bytes/bytes-from-chars "foobar")}
+        query {:plane (:plane data)
+               :metric (:metric data)
+               :bucket (:bucket data)}]
+    {:store-fn #(put-metrics-handler (assoc data :offset %))
+     :fetch-fn #(map :offset (get-metrics-handler (assoc query :offset %1 :limit %2)))
+     :mkrow-fn identity}))
