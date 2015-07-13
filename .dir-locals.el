ac4d6fde5566a4916830b46334609557ce263383
dgvncsz0f <dsouza@c0d3.xxx>
2015-07-13 19:00:35 -0300

    remove dgvncsz0f-- variables

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,13 @@
 ;;; Directory Local Variables
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
-((python-mode (py-indent-offset . 4))
- (dired-mode (dired-omit-mode . 1)))
+((nil (lice:default-license . "mit"))
+ (python-mode . ((py-indent-offset . 4)))
+ ("src/warpdrive" . ((nil . ((dgvncsz0f-compile-root . "src/warpdrive")
+                             (dgvncsz0f-compile-command . "cabal compile")
+                             (dgvncsz0f-compile-test-command . "cabal test")
+                             (dgvncsz0f-compile-clean-command . "cabal clean")))))
+ ("src/storaged" . ((nil . ((dgvncsz0f-compile-root . "src/storaged")
+                            (dgvncsz0f-compile-command . "lein with-profile storaged,triggers compile")
+                            (dgvncsz0f-compile-test-command . "lein with-profile storaged,triggers test")
+                            (dgvncsz0f-compile-clean-command . "lein with-profile storaged,triggers clean"))))))
