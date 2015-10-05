a9709e0bfbb9293885c45f92d96a96796c317147
dgvncsz0f <dsouza@c0d3.xxx>
2015-10-05 16:04:41 -0300

    python bootstrap

--- automation/bootstrap/python-bootstrap.sh
+++ automation/bootstrap/python-bootstrap.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
+set -e
+
+srcroot=${srcroot:-$(dirname $(readlink -f "$0"))}
+
+. "$srcroot/bootstrap-lib.sh"
+
 python_debian () {
   debian_install python-dev
 }
 
 python_centos () {
-  rpm_install python26-devel python-devel
+  rpm_install ruby-devel
+  rpm_install python26-devel
 }
 
 if has_command dpkg apt-get
